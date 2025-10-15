8f7fc7abecd9bfead28f4438b1ab26726a2e0c8f
estevam02009 <manoel.neto53861@alunos.ufersa.edu.br>
2025-10-15 08:56:48 -0300

    Concluido correção de erros de Login

---
+++ .history/routes/api_20251015083412.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Rota de exemplo para testar autenticação
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
